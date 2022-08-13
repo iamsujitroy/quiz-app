a85074b2be0fc7ab59a23cfea76f085e7d912223
Sujit Roy <sujitroy78148@gmail.com>
2022-08-13 09:54:00 +0530

    the app build is compleated successfuly

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const questions = [
+    {
+      questionText: 'What is the capital of India?',
+      answerOptions: [
+        { answerText: 'Mumbai', isCorrect: false },
+        { answerText: 'Uttar Pradesh', isCorrect: false },
+        { answerText: 'New Delhi', isCorrect: true },
+        { answerText: 'Gujrat', isCorrect: false },
+      ],
+    },
+    {
+      questionText: 'Who is the Prime Minister of India?',
+      answerOptions: [
+        { answerText: 'Rahul Gandhi(pappu)', isCorrect: false },
+        { answerText: 'Narendra Modi', isCorrect: true },
+        { answerText: 'Arvind Kejriwal', isCorrect: false },
+        { answerText: 'Uddhav Thackeray', isCorrect: false },
+      ],
+    },
+    {
+      questionText: 'Who is know as netaji?',
+      answerOptions: [
+        { answerText: 'Mahatma Gandhi', isCorrect: false },
+        { answerText: 'Sardar Patel', isCorrect: false },
+        { answerText: 'Subhas Chandra Bose', isCorrect: true },
+        { answerText: 'Jawaharlal Nehru', isCorrect: false },
+      ],
+    },
+    {
+      questionText: "Who is known as 'Missile Man' of India?",
+      answerOptions: [
+        { answerText: 'Kalpana Chawla', isCorrect: false },
+        { answerText: 'Vikram Sarabhai ', isCorrect: false },
+        { answerText: 'Srinivasa Ramanujan', isCorrect: false },
+        { answerText: 'APJ Abdul Kalam', isCorrect: true },
+      ],
+    },
+  ];
+  const [currentQuestion, setCurrentQuestion] = useState(0)
+  const [quizEnd, setQuizEnd] = useState(false)
+  const [score, setScore] = useState(0)
+
+  const buttonClickHandler = (isCorrect) => {
+    if (currentQuestion + 1 !== questions.length) {
+      setCurrentQuestion(currentQuestion + 1)
+    } else {
+      setQuizEnd(true)
+    }
+    if (isCorrect) {
+      setScore(score + 1)
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="quiz-container">
+
+        {!quizEnd && <div className="quiz-content">
+          <div className='left-container'>
+            <span className='question-num'>Question {currentQuestion + 1}/<span>{questions.length}</span></span>
+            <h1 className='question'>{questions[currentQuestion].questionText}</h1>
+          </div>
+          <div className='right-container'>
+            {questions[currentQuestion].answerOptions.map((val) => {
+              return <button key={val.answerText} onClick={() => buttonClickHandler(val.isCorrect)}>{val.answerText}</button>;
+            })}
+          </div>
+        </div>}
+        {quizEnd && <div className='quiz-results'>
+          <p>Your Score is {score} out of {questions.length}</p>
+        </div>}
+      </div>
     </div>
   );
 }
